71a53f4a016822c35cf7226a2f4c32574948a5fd
mudassardevr <asuspulsar3@gmail.com>
2026-02-15 20:39:48 +0530

    initial commit 🚀

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='font-bold text-2xl text-blue-500'>About</div>
+  )
+}
+
+export default About
---
+++ src/services/authService.js
@@ -0,0 +1,43 @@
+const API_URL = "http://localhost:5000/api/auth";
+
+
+//LOGIN API
+export const loginAPI = async (credentials) => {
+    
+    const response = await fetch(`${API_URL}/login`, {
+        method : "POST" ,
+        headers : {
+            "Content-Type" : "application/json",
+        }, 
+        body : JSON.stringify(credentials)
+    });
+
+    const data = await response.json();
+
+    if(!response.ok){
+        throw new Error(data.error || "Failed Login ");
+    }
+
+    return data;
+
+};
+
+// REGISTER API
+export const registerAPI = async (credentials) => {
+    const response = await fetch(`${API_URL}/register`, {
+        method : "POST" , 
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(credentials)
+    });
+
+    const data = await response.json();
+
+    if(!response.ok){
+        throw new Error(data.error || "Regsiter failed");
+    }
+
+    return data;
+
+};
